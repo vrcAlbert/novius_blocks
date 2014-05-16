dc0c6fcf91733a90dd9d81f14f9651ff12c8cac7
Jiboule <cadot@novius.fr>
2014-05-16 10:43:06 +0200

    Traduction fr

---
+++ lang/fr/default.lang.php
@@ -0,0 +1,127 @@
+<?php
+
+// Generated on 16/05/2014 10:37:39
+
+// 32 out of 32 messages are translated (100%).
+// 96 out of 96 words are translated (100%).
+
+return array(
+    #: config/controller/admin/column/crud.config.php:22
+    #: config/controller/admin/column/crud.config.php:67
+    #: config/metadata.config.php:12
+    #: config/metadata.config.php:28
+    #: config/metadata.config.php:39
+    #: config/metadata.config.php:54
+    'Blocks' => 'Blocs',
+
+    #: config/controller/admin/column/crud.config.php:41
+    'ID: ' => '',
+
+    #. Mandatory title
+    #. We set the transmitted datas
+    #: config/controller/admin/column/crud.config.php:48
+    #: config/controller/admin/block/crud.config.php:141
+    #: config/connection_model.config.php:24
+    #: config/common/block.config.php:15
+    #: views/admin/block/template.view.php:29
+    'Title' => 'Titre',
+
+    #: config/controller/admin/column/crud.config.php:89
+    #: config/controller/admin/block/crud.config.php:243
+    #: views/admin/block/retrieve_model.view.php:71
+    'Save' => 'Enregistrer',
+
+    #: config/controller/admin/block/inspector/column.config.php:28
+    #: config/controller/admin/block/crud.config.php:107
+    #: config/controller/admin/block/crud.config.php:187
+    #: config/common/column.config.php:14
+    'Columns' => 'Colonnes',
+
+    #: config/controller/admin/block/crud.config.php:25
+    'Template' => '',
+
+    #: config/controller/admin/block/crud.config.php:43
+    #: config/controller/admin/block/crud.config.php:159
+    'Image' => '',
+
+    #: config/controller/admin/block/crud.config.php:62
+    #: config/controller/admin/block/crud.config.php:148
+    #: config/connection_model.config.php:28
+    'Description' => '',
+
+    #: config/controller/admin/block/crud.config.php:82
+    #: config/controller/admin/block/crud.config.php:166
+    #: views/admin/block/template.view.php:33
+    'Link' => 'Lien',
+
+    #: config/controller/admin/block/crud.config.php:113
+    'Link with a model' => 'Associer à un model',
+
+    #: config/controller/admin/block/crud.config.php:122
+    'Configuration' => '',
+
+    #: config/controller/admin/block/crud.config.php:170
+    'Text of the link' => 'Texte du lien',
+
+    #: config/controller/admin/block/crud.config.php:173
+    'Open in a new page' => 'Ouvrir dans une nouvelle page',
+
+    #: config/controller/admin/block/crud.config.php:180
+    'Class' => '',
+
+    #: config/controller/admin/block/crud.config.php:219
+    'Search the item' => 'Chercher l\'élément',
+
+    #: config/connection_model.config.php:14
+    'A page' => 'Une page',
+
+    #: config/metadata.config.php:55
+    'Put forward this element' => 'Mettez cet élément en avant',
+
+    #: config/metadata.config.php:60
+    'Block' => 'Bloc',
+
+    #: classes/controller/admin/block/enhancer.ctrl.php:27
+    'I’m an application. Give me a name!' => 'Je suis une application, donnez moi un nom !',
+
+    #: views/admin/enhancer/popup.view.php:66
+    'Display type' => 'type d\'affichage',
+
+    #: views/admin/enhancer/popup.view.php:68
+    #: views/admin/enhancer/popup.view.php:90
+    'Column' => 'Colonne',
+
+    #: views/admin/enhancer/popup.view.php:69
+    'Blocks selection' => 'Séléction de blocs',
+
+    #: views/admin/enhancer/preview.view.php:12
+    'No block will be displayed' => 'Aucun bloc ne sera affiché',
+
+    #: views/admin/block/retrieve_model.view.php:22
+    'Retrieve the datas of an item' => 'Récupérer les données d\'un objet',
+
+    #: views/admin/block/retrieve_model.view.php:63
+    'Use the url of an item' => 'Utiliser l\'url d\'un objet',
+
+    #: views/admin/block/model_compatibility.view.php:17
+    'Search for an item to synchronize :' => 'Chercher un objet à synchroniser',
+
+    #: views/admin/block/model_compatibility.view.php:18
+    'Title :' => 'Titre :',
+
+    #: views/admin/block/model_compatibility.view.php:20
+    'Last elements added :' => 'Derniers éléments ajoutés :',
+
+    #: views/admin/block/connection_model.view.php:23
+    'Link with :' => 'Lier avec :',
+
+    #: views/admin/block/model_assoc_infos.view.php:12
+    'This block is linked to an item ' => 'Ce bloc est lié à un objet ',
+
+    #: views/admin/block/model_assoc_infos.view.php:14
+    'Title of the item :' => 'Titre de l\'objet :',
+
+    #: views/admin/block/model_assoc_infos.view.php:19
+    'Delete this link' => 'Supprimer ce lien',
+
+);
